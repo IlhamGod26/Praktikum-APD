459510ef34a582fdedba97243853a4da3df5e3cb
IlhamGod26 <ridwanali731@gmail.com>
2024-10-01 13:02:27 +0100

    upload posttest

---
+++ posttest 4/2409106114_Riduan_Ali_c2'24_posttest 4.py
@@ -0,0 +1,38 @@
+hitung = 0
+username_benar = "riduan"  
+password_benar = 14        
+
+while hitung < 3: 
+   
+    username_input = input("Masukkan username: ")
+    password_input = input("Masukkan password: ")
+
+    if username_input == username_benar and int(password_input) == password_benar:
+        print("Login Berhasil!")
+        break 
+    else:
+        hitung += 1  
+        print(f"Username atau password salah. Percobaan {hitung} dari 3.")
+        
+       
+        if hitung == 3:
+            print("Login gagal, sudah 3 kali percobaan.")
+            exit("Login gagal")
+
+jenismobil = input("Masukkan jenis mobil (tesla, toyota, hyundai):")
+harga = int(input("Masukkan harga mobil:"))
+
+if jenismobil == "tesla":
+    diskon = harga * 0.17
+    hsd = harga - diskon
+    print(f"Harga mobil Tesla setelah diskon: {hsd}")
+elif jenismobil == "toyota":
+    diskon = harga * 0.21
+    hsd = harga - diskon
+    print(f"Harga mobil Toyota setelah diskon: {hsd}")
+elif jenismobil == "hyundai":
+    diskon = harga * 0.23
+    hsd = harga - diskon
+    print(f"Harga mobil Hyundai setelah diskon: {hsd}")
+else:
+    print("tidak jadi beli mobil")
